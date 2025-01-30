4f522e797a79d5aed6320d5a1d6916fdf051cdde
Nesodev <ever.avendano@unmsm.edu.pe>
2025-01-30 17:07:26 -0500

    test-integration

--- lab02/application/backend/app.py
+++ lab02/application/backend/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import requests
-from application.backend.db.model import Alumno, CarreraProfesional
+from application.db.model import Alumno, CarreraProfesional
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
